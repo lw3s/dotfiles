973b625862ac916ee2eea1900b309a756b9fd45b
Lucas Grenier <luc@sgrenier.com>
2025-04-30 15:38:13 -0400

    1

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,12 @@
-require("lazy-setup")
+require("packages")
 require("remap")
 require("lsps")
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = false
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 
--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -2,14 +2,19 @@ local nnrm = function(new, old) -- nnrm = normal-no-recurse-map
     vim.keymap.set("n", new, old, { noremap = true, silent = true })
 end
 
+local vnrm = function(new, old) -- vnrm = visual-no-recurse-map
+    vim.keymap.set("v", new, old, { noremap = true, silent = true })
+end
+
+local inrm = function(new, old) -- inrm = insert-no-recurse-map
+    vim.keymap.set("i", new, old, { noremap = true, silent = true })
+end
+
 local nvnrm = function(new, old) -- nvnrm = normal/visual-no-recurse-map
     vim.keymap.set("n", new, old, { noremap = true, silent = true })
     vim.keymap.set("v", new, old, { noremap = true, silent = true })
 end
 
-nnrm(" 1", "0i")
-nnrm(" 0", "$i<Right>")
-nnrm(" p", "\"0p")
 nnrm("-", "<CMD>Oil<CR>")
 nnrm("<leader>e", vim.diagnostic.open_float)
 nnrm("<leader>r", vim.lsp.buf.rename)
@@ -26,5 +31,17 @@ nvnrm("W", "w")
 nvnrm("b", "B")
 nvnrm("B", "b")
 
-vim.keymap.set("i", "<esc>", "<Right><esc>")
+nvnrm("j", "h")
+nvnrm("k", "j")
+nvnrm("l", "k")
+nvnrm(";", "l")
+
+nvnrm("<leader>1", "0i")
+nvnrm("<leader>0", "$i<Right>")
+
+nnrm("P", "\"0p")
+nnrm("<leader>p", "\"*p")
+vnrm("<leader>y", "\"*y")
+
+inrm("<esc>", "<Right><esc>")
 
